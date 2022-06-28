9aa8eba2f55f04ea1e0f2ef25dc43973897db128
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-28 20:35:23 -0300

    first commit

---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,5 @@
+import { Routes, Route } from 'react-router-dom'
+
+export function AuthRoutes() {
+  return <h1>Auth</h1>
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#1C1B1E',
+
+    GRAY: '#3E3B47',
+    RED: '#FF859B',
+    INPUT: '#262529',
+    TAG: '#312E38',
+    CARD: '#FF859B0D',
+
+    TEXT_WHITE: '#F4EDE8',
+    TEXT_LIGHT_GRAY: '#999591',
+    TEXT_GRAY: '#948F99'
+  }
+}
