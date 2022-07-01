085327864143a31f17c5fc9c35d394bad33cfa4d
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-30 21:24:51 -0300

    Movie Preview Page done

---
+++ src/pages/MoviePreview/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0 123px;
+  margin-top: 40px;
+
+  > header {
+    display: flex;
+    justify-content: flex-start;
+    margin-bottom: 24px;
+  }
+
+  > .title {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 19px;
+    margin-bottom: 24px;
+
+    h1 {
+      font-size: 36px;
+      font-weight: 500;
+      line-height: 47px;
+    }
+  }
+
+  > .author {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 18.75px;
+    margin-bottom: 40px;
+
+    .name,
+    .date {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+
+    .name img {
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      border: 1px solid ${({ theme }) => theme.COLORS.GRAY};
+    }
+
+    .date svg {
+      color: ${({ theme }) => theme.COLORS.RED};
+    }
+  }
+
+  > .tags {
+    display: flex;
+    gap: 8px;
+    margin-bottom: 40px;
+  }
+
+  > .description {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 21px;
+    text-align: justify;
+
+    display: flex;
+    flex-direction: column;
+    gap: 21px;
+  }
+`
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Profile } from '../pages/Profile'
 import { CreateMovie } from '../pages/CreateMovie'
+import { MoviePreview } from '../pages/MoviePreview'
 
 export function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/create" element={<CreateMovie />} />
+      <Route path="/preview" element={<MoviePreview />} />
     </Routes>
   )
 }
