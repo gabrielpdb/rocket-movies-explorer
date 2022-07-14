05db60ec917224b725f8c38428d7097dde300adb
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-07-13 21:08:35 -0300

    started to code front here

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,8 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Routes />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Routes />
+  </ThemeProvider>
 )
