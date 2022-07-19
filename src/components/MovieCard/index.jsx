b8a0e64c8f52e207fc6847188398dd0f1e3c122b
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-07-19 07:04:17 -0300

    home done

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,18 +1,26 @@
 import { Tag } from '../Tag'
 import { Container } from './styles'
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import { useEffect, useState } from 'react'
 
-export function MovieCard({ title, description, tags }) {
+export function MovieCard({ title, description, tags, rating }) {
+  const [stars, setStars] = useState([])
+
+  useEffect(() => {
+    setStars([])
+    for (let index = 1; index <= 5; index++) {
+      if (index <= rating) {
+        setStars(prevStars => [...prevStars, <AiFillStar size={12} />])
+      } else {
+        setStars(prevStars => [...prevStars, <AiOutlineStar size={12} />])
+      }
+    }
+  }, [])
+
   return (
     <Container>
       <h2>{title}</h2>
-      <div className="stars">
-        <AiFillStar size={12} />
-        <AiFillStar size={12} />
-        <AiFillStar size={12} />
-        <AiFillStar size={12} />
-        <AiOutlineStar size={12} />
-      </div>
+      <div className="stars">{stars}</div>
 
       <p>{description}</p>
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,13 +4,23 @@ import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { IoMdAdd } from 'react-icons/io'
 import { MovieCard } from '../../components/MovieCard'
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+import { useNavigate } from 'react-router-dom'
 
 export function Home() {
-  const tags = [
-    { id: 1, name: 'Ficção Científica' },
-    { id: 2, name: 'Drama' },
-    { id: 3, name: 'Suspense' }
-  ]
+  const [notes, setNotes] = useState([])
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/movie_notes?title=${search}`)
+      setNotes(response.data)
+    }
+
+    fetchNotes()
+  }, [])
 
   return (
     <>
@@ -21,26 +31,18 @@ export function Home() {
           <Button title="Adicionar filme" icon={IoMdAdd} />
         </div>
         <div className="moviesList">
-          <MovieCard
-            title="Interestellar"
-            description="Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-            tags={tags}
-          />
-          <MovieCard
-            title="Interestellar"
-            description="Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-            tags={tags}
-          />
-          <MovieCard
-            title="Interestellar"
-            description="Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-            tags={tags}
-          />
-          <MovieCard
-            title="Interestellar"
-            description="Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se..."
-            tags={tags}
-          />
+          {notes &&
+            notes.map(note => {
+              return (
+                <MovieCard
+                  key={note.id}
+                  title={note.title}
+                  description={note.description}
+                  rating={note.rating}
+                  tags={note.tags}
+                />
+              )
+            })}
         </div>
       </Container>
     </>
